1e6be6aae085f4aac1b3c4b069e3db32683bb72c
Denis Kormalev <kormalev.denis@gmail.com>
2019-03-01 08:58:02 -0800

    Better test coverage for asynqro wrapping [AB#188111] (#8)

--- tests/proofseed/tasks_test.cpp
+++ tests/proofseed/tasks_test.cpp
@@ -13,6 +13,17 @@
 using namespace Proof;
 using namespace Proof::tasks;
 
+TEST(TasksTest, emptySignalWaiting)
+{
+    Future<int> future = run([]() {
+        fireSignalWaiters();
+        return 42;
+    });
+    future.wait(1000);
+    ASSERT_TRUE(future.isCompleted());
+    EXPECT_EQ(42, future.result());
+}
+
 TEST(TasksTest, signalWaiting)
 {
     QThread thread;
@@ -36,6 +47,51 @@ TEST(TasksTest, signalWaiting)
         ;
     EXPECT_FALSE(future.isCompleted());
     QMetaObject::invokeMethod(timer, "start", Qt::BlockingQueuedConnection, Q_ARG(int, 1));
+    future.wait(1000);
+    ASSERT_TRUE(future.isCompleted());
+    EXPECT_EQ(42, future.result());
+    thread.quit();
+    thread.wait(100);
+    delete timer;
+}
+
+TEST(TasksTest, multipleSignalWaiting)
+{
+    QThread thread;
+    thread.start();
+    std::atomic_bool ready{false};
+    std::atomic_bool ready2{false};
+    QTimer *timer = new QTimer;
+    timer->setSingleShot(true);
+    timer->moveToThread(&thread);
+    QTimer *timer2 = new QTimer;
+    timer2->setSingleShot(true);
+    timer2->moveToThread(&thread);
+    Future<int> future = run([timer, timer2, &ready, &ready2]() {
+        int result = 0;
+        std::function<bool()> timerCallback = [&result, &ready2]() {
+            result = 21;
+            ready2 = true;
+            return false;
+        };
+        std::function<bool()> timer2Callback = [&result]() {
+            result = 42;
+            return true;
+        };
+        addSignalWaiter(timer, &QTimer::timeout, timerCallback);
+        addSignalWaiter(timer2, &QTimer::timeout, timer2Callback);
+        ready = true;
+        fireSignalWaiters();
+        return result;
+    });
+    while (!ready)
+        ;
+    EXPECT_FALSE(future.isCompleted());
+    QMetaObject::invokeMethod(timer, "start", Qt::BlockingQueuedConnection, Q_ARG(int, 1));
+    while (!ready2)
+        ;
+    ASSERT_FALSE(future.isCompleted());
+    QMetaObject::invokeMethod(timer2, "start", Qt::BlockingQueuedConnection, Q_ARG(int, 1));
     future.wait(1000);
     ASSERT_TRUE(future.isCompleted());
     EXPECT_EQ(42, future.result());
