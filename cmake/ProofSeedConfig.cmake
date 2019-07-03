f2292d585d507bf5e79c749231098bfe05061e40
Denis Kormalev <denis.kormalev@opensoftdev.com>
2019-07-03 09:50:41 -0700

    Extra cmake dependency (qtconcurrent) required by asynqro added to seedconfig

--- cmake/ProofSeedConfig.cmake
+++ cmake/ProofSeedConfig.cmake
@@ -2,6 +2,7 @@ include(CMakeFindDependencyMacro)
 
 list(APPEND CMAKE_PREFIX_PATH "${CMAKE_CURRENT_LIST_DIR}/3rdparty")
 find_dependency(Qt5Core CONFIG REQUIRED)
+find_dependency(Qt5Concurrent CONFIG REQUIRED)
 find_dependency(asynqro CONFIG REQUIRED)
 list(REMOVE_AT CMAKE_PREFIX_PATH -1)
 
