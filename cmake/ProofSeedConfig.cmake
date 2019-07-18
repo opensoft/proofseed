e5a36d981ff84797ab2685914bab4a5bd2da45c4
Denis Kormalev <denis.kormalev@opensoftdev.com>
2019-07-18 16:31:26 -0700

    build fix

--- cmake/ProofSeedConfig.cmake
+++ cmake/ProofSeedConfig.cmake
@@ -1,6 +1,7 @@
 include(CMakeFindDependencyMacro)
 
 list(APPEND CMAKE_PREFIX_PATH "${CMAKE_CURRENT_LIST_DIR}/3rdparty")
+find_dependency(Threads REQUIRED)
 find_dependency(Qt5Core CONFIG REQUIRED)
 find_dependency(Qt5Concurrent CONFIG REQUIRED)
 find_dependency(asynqro CONFIG REQUIRED)
